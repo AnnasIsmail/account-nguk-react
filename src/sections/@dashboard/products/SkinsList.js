646434cc6302b1ff2ee417696b387fb5b8ee9d45
Ngabjago <86274500+AnnasIsmail@users.noreply.github.com>
2022-08-14 23:18:42 +0700

    Update SkinsList.js

--- src/sections/@dashboard/products/SkinsList.js
+++ src/sections/@dashboard/products/SkinsList.js
@@ -9,6 +9,8 @@ export default function SkinsList({ products, ...other }) {
 
   const countPage = Math.floor(products.length / 20);
   const [currentPage , setCurrentPage] = React.useState(1);
+  const [indexFrom , setIndexFrom] = React.useState(-1);
+  const [indexTo , setIndexTo] = React.useState(20);
 
  const  productsReady = [];
   products.forEach(element => {
@@ -18,18 +20,24 @@ export default function SkinsList({ products, ...other }) {
   });
 
   const changePage =(e)=>{
-    console.log(parseInt(e.target.innerText, 10));
+    const page = parseInt(e.target.innerText, 10);
+    const indexMax = page * 20;
+    setCurrentPage(page);
+    setIndexTo(indexMax);
+    setIndexFrom(indexMax - 21);
   }
 
   return (
-    <Grid container spacing={3} {...other}>
-      {productsReady.map((product , index) => (
-        (index > 19 && index < 40)&&
-        <Grid key={product.uuid} item xs={12} sm={6} md={3}>
-          <SkinsCard product={product} />
-        </Grid>
-      ))}
-      <Pagination count={countPage} shape="rounded" onClick={changePage} sx={{ mx: 'auto' , my: 3}} />
-    </Grid>
+    <>
+      <Grid container spacing={3} {...other}>
+        {productsReady.map((product , index) => (
+          (index > indexFrom && index < indexTo)&&
+          <Grid key={product.uuid} item xs={12} sm={6} md={3}>
+            <SkinsCard product={product} />
+          </Grid>
+        ))}
+      </Grid>
+        <Pagination count={countPage} shape="rounded" onClick={changePage} sx={{ mx: 'auto' , my: 3}} />
+    </>
   );
 }
